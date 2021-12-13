6355c5424b3ad278b82acac689ef80839e1b07b6
jylim <jylim@crscube.co.kr>
2021-12-13 12:13:20 +0900

    change variable name

--- index.py
+++ index.py
@@ -10,16 +10,15 @@ if os.environ.get("TableLayoutDirByJYLIM") is None:
     envDir = input("Table Layout을 옮길 위치를 입력해주세요 : ")
     subprocess.run(f'$Env:TableLayoutDirByJYLIM {envDir}')
 
-isSearched = False
-
 path = os.environ.get("TableLayoutPathByJYLIM")
 dir = os.environ.get("TableLayoutDirByJYLIM")
+
+isSearched = False
+isPersent = False
 
 tableName = input('테이블 이름을 입력해주세요 : ').upper()
 
 fileNameArr = tableName.split(',')
-
-isPersent = False
 
 for idx, i in enumerate(fileNameArr, 0):
     lastText = i.__len__() - 1
@@ -30,15 +29,15 @@ for idx, i in enumerate(fileNameArr, 0):
 
 file_list = os.listdir(path)
 
-for i in file_list :
-    for file in fileNameArr:
-        if file in i and isPersent:
-            print(i)
-            shutil.copy(path + i, dir + i)
+for file in file_list :
+    for fileName in fileNameArr:
+        if fileName in file and isPersent:
+            print(file)
+            shutil.copy(path + file, dir + file)
             isSearched = True
-        elif file + ".xlsx" in i:
-            print(i)
-            shutil.copy(path + i, dir + i)
+        elif fileName + ".xlsx" in file:
+            print(file)
+            shutil.copy(path + file, dir + file)
             isSearched = True
 
 if isSearched is False :
